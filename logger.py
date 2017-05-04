ff3def6d197640271cd0b716e6b7b6839d70ff49
Steffen Arntz <winrootkit@gmail.com>
2017-05-04 13:33:14 +0200

    init

---
+++ logger.py
@@ -0,0 +1,62 @@
+import sqlite3
+import paho.mqtt.client as mqtt
+import time
+
+def isfloat(value):
+  try:
+    float(value)
+    return True
+  except ValueError:
+    return False
+
+def insert_entry_in_db(topic, message, timestamp=None):
+    if(timestamp is None):
+        timestamp = int(time.time())
+    db.execute("INSERT or IGNORE INTO topics (topic) VALUES (?)", [topic])
+
+    cursor = db.execute("SELECT id FROM topics WHERE topic = ?", [topic])
+    topic_id = cursor.fetchone()[0]
+    db.execute("INSERT INTO log_entries (timestamp, value, topic_id) VALUES (?,?,?)", [timestamp, message, topic_id])
+    db.commit()
+    print("inserted")
+    return
+
+def on_connect(client, userdata, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("sensors/+/+")
+
+def on_message(client, userdata, msg):
+    payload = bytes(msg.payload).decode('utf-8')
+    topic = msg.topic
+    print(topic+" "+ payload)
+    insert_entry_in_db(topic, payload)
+    if(payload.isnumeric()):
+        print("is int")
+    else:
+        if(isfloat(payload)):
+            print("is float")
+        else:
+            print("is not a number")
+
+db = sqlite3.connect("logger.db")
+client = mqtt.Client()
+db.execute("CREATE TABLE "
+           "IF NOT EXISTS "
+           "topics ("
+           "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+           "topic TEXT UNIQUE)")
+db.execute("CREATE TABLE "
+           "IF NOT EXISTS "
+           "log_entries ("
+           "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+           "timestamp DATE, "
+           "value TEXT(25), "
+           "topic_id INTEGER, "
+           "FOREIGN KEY(topic_id) REFERENCES topics(id))")
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("127.0.0.1", 1883, 60)
+
+client.loop_forever()
